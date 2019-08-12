31d19d6abc2896c84ae4e40b3e43d2071a50a7ad
ITyunqing <1186355422@qq.com>
2019-08-12 15:40:06 +0800

    init

---
+++ src/main/java/com/java4all/IdempotentApplication.java
@@ -0,0 +1,13 @@
+package com.java4all;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdempotentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdempotentApplication.class, args);
+	}
+
+}
