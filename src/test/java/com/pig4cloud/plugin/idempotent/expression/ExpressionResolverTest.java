9e44741867bc94d9ab33ff48210edf2f558b59eb
lbw <wangiegie@163.com>
2024-01-02 13:38:42 +0800

    :arrow_up: springboot version and add params ExpressionResolverTest test

---
+++ src/test/java/com/pig4cloud/plugin/idempotent/expression/ExpressionResolverTest.java
@@ -0,0 +1,21 @@
+package com.pig4cloud.plugin.idempotent.expression;
+
+import com.pig4cloud.plugin.idempotent.controller.DemoController;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.DefaultParameterNameDiscoverer;
+import org.springframework.core.ParameterNameDiscoverer;
+
+class ExpressionResolverTest {
+
+    private static final ParameterNameDiscoverer DISCOVERER = new DefaultParameterNameDiscoverer();
+
+
+    @Test
+    public void testParameterNameDiscoverer() throws Exception{
+        String[] parameterNames = DISCOVERER.getParameterNames(DemoController.class.getDeclaredMethod("get",String.class));
+
+        for (String parameterName : parameterNames) {
+            System.out.println(parameterName);
+        }
+    }
+}
